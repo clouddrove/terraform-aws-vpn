9d3bcb334034ffe614acaa18bd60bad664129511
Anmol Nagpal <4303310+anmolnagpal@users.noreply.github.com>
2019-05-01 13:18:06 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,84 @@
+variable "Name" {
+  description = "Name  (e.g. `app` or `cluster`)"
+  type        = "string"
+}
+
+variable "Environment" {
+  type        = "string"
+  description = "Environment (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "CreatedBy" {
+  type        = "string"
+  description = "CreatedBy (e.g. `terraform`, `dev`, `devops`)"
+}
+
+variable "Organization" {
+  type        = "string"
+  description = "Organization (e.g. `bac`, `cd`)"
+}
+
+variable "vpn_gateway_id" {
+  description = "The id of the VPN Gateway."
+}
+
+variable "customer_gateway_id" {
+  description = "The id of the Customer Gateway."
+}
+
+variable "create_vpn_connection" {
+  description = "Set to false to prevent the creation of a VPN Connection."
+  default     = true
+}
+
+variable "vpc_id" {
+  description = "The id of the VPC where the VPN Gateway lives."
+}
+
+variable "vpc_subnet_route_table_ids" {
+  description = "The ids of the VPC subnets for which routes from the VPN Gateway will be propagated."
+  type        = "list"
+  default     = []
+}
+
+variable "vpc_subnet_route_table_count" {
+  description = "The number of subnet route table ids being passed in via `vpc_subnet_route_table_ids`."
+  default     = 0
+}
+
+variable "vpn_connection_static_routes_only" {
+  description = "Set to true for the created VPN connection to use static routes exclusively (only if `create_vpn_connection = true`). Static routes must be used for devices that don't support BGP."
+  default     = false
+}
+
+variable "vpn_connection_static_routes_destinations" {
+  description = "List of CIDRs to be used as destination for static routes (used with `vpn_connection_static_routes_only = true`). Routes to destinations set here will be propagated to the routing tables of the subnets defined in `vpc_subnet_route_table_ids`."
+  type        = "list"
+  default     = []
+}
+
+variable "tunnel1_inside_cidr" {
+  description = "The CIDR block of the inside IP addresses for the first VPN tunnel."
+  default     = ""
+}
+
+variable "tunnel2_inside_cidr" {
+  description = "The CIDR block of the inside IP addresses for the second VPN tunnel."
+  default     = ""
+}
+
+variable "tunnel1_preshared_key" {
+  description = "The preshared key of the first VPN tunnel."
+  default     = ""
+}
+
+variable "tunnel2_preshared_key" {
+  description = "The preshared key of the second VPN tunnel."
+  default     = ""
+}
+
+#Attachment can be already managed by the terraform-aws-vpc module by using the enable_vpn_gateway variable
+variable "create_vpn_gateway_attachment" {
+  description = "Set to false to prevent attachment of the vGW to the VPC"
+  default     = true
+}
