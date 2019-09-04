ac0d4e348ab25659964b2cfcb5cdf4c8cd50f61c
Sohan Yadav <sohan@clouddrove.com>
2019-09-04 17:25:50 +0530

    upgrade0.12

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,40 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "vpc" {
+  source = "git::https://github.com/clouddrove/terraform-aws-vpc.git"
+
+  name        = "vpc"
+  application = "clouddrove"
+  environment = "test"
+  label_order = ["environment", "name", "application"]
+
+  cidr_block = "172.16.0.0/16"
+}
+
+module "public_subnets" {
+  source = "git::https://github.com/clouddrove/terraform-aws-subnet.git"
+
+  name        = "public-subnet"
+  application = "clouddrove"
+  environment = "test"
+  label_order = ["environment", "name", "application"]
+
+  availability_zones = ["eu-west-1b", "eu-west-1c"]
+  vpc_id             = module.vpc.vpc_id
+  cidr_block         = module.vpc.vpc_cidr_block
+  type               = "public"
+  igw_id             = module.vpc.igw_id
+}
+
+
 module "vpn" {
-  source              = "./../../terraform-aws-vpn"
-  name                = "vpn"
-  application        = "Clouddrove"
-  environment         = "test"
-  createdby           = "Anmol"
-  vpc_id              = "vpc-XXXXXXXXX"
-  customer_ip_address = "0.0.0.0"
+  source              = "./../"
+  name        = "vpn"
+  application = "clouddrove"
+  environment = "test"
+  label_order = ["environment", "name", "application"]
+  vpc_id              = module.vpc.vpc_id
+  customer_ip_address = "115.160.246.74"
 }
